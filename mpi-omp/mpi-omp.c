a5220606cff9ca1363596380491635013e71341d
Gabriel Ivascu <ivascu.gabriel59@gmail.com>
2016-12-13 12:02:24 +0200

    Remove unused includes

--- mpi-omp/mpi-omp.c
+++ mpi-omp/mpi-omp.c
@@ -1,12 +1,9 @@
 #include <assert.h>
-#include <float.h>
 #include <math.h>
 #include <omp.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "../libde/de.h"
 #include "mpi.h"
--- serial/serial.c
+++ serial/serial.c
@@ -1,11 +1,8 @@
 #include <assert.h>
-#include <float.h>
 #include <math.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "../libde/de.h"
 #include "utils.h"
